f0e3ba32dd04bdef360804c7aeec007d8f36eccf
Roy Triesscheijn <roy-t@hotmail.com>
2021-04-10 14:47:09 +0200

    Add simple content file system

---
+++ src/ShaderTools.CodeAnalysis.Hlsl/Text/ContentFileSystem.cs
@@ -0,0 +1,15 @@
+﻿using System.IO;
+using Microsoft.CodeAnalysis.Text;
+
+namespace ShaderTools.CodeAnalysis.Hlsl.Text
+{
+    public sealed class ContentFileSystem : IIncludeFileSystem
+    {
+        public bool TryGetFile(string path, out SourceText text)
+        {
+            var sourceCode = File.ReadAllText(path);
+            text = SourceText.From(sourceCode);
+            return true;
+        }
+    }
+}
